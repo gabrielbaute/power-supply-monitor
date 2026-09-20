6e4e610c1b9aa702307d7bca5426786807de2039
gabrielbaute <gabrielbaute@gmail.com>
2026-09-20 15:08:59 -0400

    Exportamos la isntancia común del database manager

--- app/managers/__init__.py
+++ app/managers/__init__.py
@@ -1,2 +1,2 @@
-from app.managers.database_manager import DatabaseManager
+from app.managers.database_manager import DatabaseManager, db_manager
 from app.managers.power_monitor_manager import PowerMonitorManager
